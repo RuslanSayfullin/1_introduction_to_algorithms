d6cfd1133ac7363385513620283d5ed12a7342a2
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2019-10-31 20:15:26 +0500

    First

---
+++ 0.simple_number.py
@@ -0,0 +1,13 @@
+def is_simple_number(x):
+    """определяет, является ли число простым.
+    x- целое положительное число.
+    Если простое, то возврощает True.
+    а иначе False
+    """
+
+    divisor = 2
+    while divisor < x:
+        if x%divisor ==0:
+            return False
+        divisor += 1
+    return True
